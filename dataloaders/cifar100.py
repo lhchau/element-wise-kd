688a8329e4957fae142e2672fca05bdd675362d9
lhchau <lhchau20@apcs.fitus.edu.vn>
2025-05-24 22:04:35 +0700

    modify: num_worker to 4

--- dataloaders/cifar100.py
+++ dataloaders/cifar100.py
@@ -4,7 +4,7 @@ import torchvision.transforms as transforms
 
 def get_cifar100(
     batch_size=128,
-    num_workers=2):
+    num_workers=4):
     transform_train = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
@@ -21,6 +21,6 @@ def get_cifar100(
     data_test = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform_test)
     
     train_dataloader = torch.utils.data.DataLoader(data_train, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True)
-    test_dataloader = torch.utils.data.DataLoader(data_test, batch_size=500, shuffle=False, num_workers=2, pin_memory=True)
+    test_dataloader = torch.utils.data.DataLoader(data_test, batch_size=500, shuffle=False, num_workers=4, pin_memory=True)
     
     return train_dataloader, test_dataloader, len(data_test.classes)
