ef23079ca22dd952c2e3e6b1208d51d10e33614d
lhchau <lhchau20@apcs.fitus.edu.vn>
2025-06-20 11:10:26 +0700

    feat: training imagenet

--- dataloaders/__init__.py
+++ dataloaders/__init__.py
@@ -2,6 +2,7 @@ from .cifar100 import get_cifar100
 from .cifar10 import get_cifar10
 from .tiny_imagenet import get_tiny_imagenet
 from .cifar100_super_class import get_cifar100_super_class
+from .imagenet import *
 
 # Data
 def get_dataloader(
@@ -18,3 +19,5 @@ def get_dataloader(
         return get_tiny_imagenet(batch_size, num_workers)
     elif data_name == "cifar100_super_class":
         return get_cifar100_super_class(batch_size, num_workers)
+    elif data_name == 'imagenet':
+        return get_imagenet_dataloaders(batch_size=batch_size, val_batch_size=128, num_workers=num_workers)
--- train_student.py
+++ train_student.py
@@ -26,7 +26,6 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc, logging_dict = 0, {}
 EPOCHS = cfg['trainer']['epochs'] 
 teacher_path = cfg['trainer'].get('teacher_path', None)
-assert teacher_path is not None, "Teacher path should not be None"
 # KL Hyperparameters
 
 kd_type = cfg['trainer'].get('kd_type', 'kd')
@@ -34,6 +33,7 @@ T = cfg['trainer'].get('T', 4)
 kl_weight = cfg['trainer'].get('kl_weight', 1)
 ce_weight = cfg['trainer'].get('ce_weight', 1)
 rho = cfg['trainer'].get('rho', None)
+warmup = cfg['trainer'].get('warmup', 1)
 dkd_beta = cfg['trainer'].get('dkd_beta', None)
 pprint.pprint(cfg)
 
@@ -42,7 +42,7 @@ pprint.pprint(cfg)
 ################################
 print('==> Initialize Logging Framework..')
 logging_name = get_logging_name(cfg)
-logging_name += (f'_T={T}' + f'_kl_w={kl_weight}' + f'_ce_w={ce_weight}' + f'_kd_t={kd_type}' + f'_rho={rho}' + f'_seed={seed}' + f'_{current_time}')
+logging_name += (f'_T={T}' + f'_kl_w={kl_weight}' + f'_ce_w={ce_weight}' + f'_kd_t={kd_type}' + f'_rho={rho}' + f'_wup={warmup}' + f'_seed={seed}' + f'_{current_time}')
 if kd_type == 'dkdadakd':
     logging_name += f'_dkd_be={dkd_beta}'
 framework_name = cfg['logging']['framework_name']
@@ -62,22 +62,26 @@ train_dataloader, test_dataloader, num_classes = get_dataloader(**cfg['dataloade
 #### 2.1 Teacher
 ################################
 teacher_model_name = cfg['teacher_model'].pop('model_name', None)
-teacher_model = model_dict[teacher_model_name](num_classes=num_classes, **cfg['teacher_model'])
-teacher_model = teacher_model.to(device)
-# Teacher - Load checkpoint
-print(f'==> Load checkpoint for Teacher: {teacher_model_name}')
-assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-checkpoint = torch.load(teacher_path, weights_only=False)
-teacher_model.load_state_dict(checkpoint['model'])
-
-################################
-#### 2.2 Student
-################################
 student_model_name = cfg['student_model'].pop('model_name', None)
-student_model = model_dict[student_model_name](num_classes=num_classes, **cfg['student_model'])
-student_model = student_model.to(device)
-total_params = sum(p.numel() for p in student_model.parameters())
-print(f'==> Number of parameters in Student: {student_model_name}: {total_params}')
+if cfg['dataloader']['data_name'] == "imagenet":
+    teacher_model = imagenet_model_dict[teacher_model_name](pretrained=True)
+    model_student = imagenet_model_dict[student_model_name](pretrained=False)
+else:
+    teacher_model = model_dict[teacher_model_name](num_classes=num_classes, **cfg['teacher_model'])
+    teacher_model = teacher_model.to(device)
+    # Teacher - Load checkpoint
+    print(f'==> Load checkpoint for Teacher: {teacher_model_name}')
+    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+    checkpoint = torch.load(teacher_path, weights_only=False)
+    teacher_model.load_state_dict(checkpoint['model'])
+
+    ################################
+    #### 2.2 Student
+    ################################
+    student_model = model_dict[student_model_name](num_classes=num_classes, **cfg['student_model'])
+    student_model = student_model.to(device)
+    total_params = sum(p.numel() for p in student_model.parameters())
+    print(f'==> Number of parameters in Student: {student_model_name}: {total_params}')
 
 ################################
 #### 3.a OPTIMIZING MODEL PARAMETERS
@@ -85,7 +89,7 @@ print(f'==> Number of parameters in Student: {student_model_name}: {total_params
 if kd_type == 'kd':
     criterion = KD(T=T, kl_weight=kl_weight, ce_weight=ce_weight)
 elif kd_type == 'adakd':
-    criterion = ADAKD(kl_weight=kl_weight, ce_weight=ce_weight, rho=rho)
+    criterion = ADAKD(kl_weight=kl_weight, ce_weight=ce_weight, rho=rho, warmup=warmup)
 elif kd_type == 'dkdadakd':
     criterion = DKDADAKD(kl_weight=kl_weight, ce_weight=ce_weight, rho=rho, dkd_beta=dkd_beta)
 elif kd_type == 'adakd_gap':
@@ -100,7 +104,10 @@ elif kd_type == 'rho_free':
 test_criterion = nn.CrossEntropyLoss()
 opt_name = cfg['optimizer'].pop('opt_name', None)
 optimizer = get_optimizer(student_model, opt_name, cfg['optimizer'])
-scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[150, 180, 210], gamma=0.1)
+if cfg['dataloader']['data_name'] == "imagenet":
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[30, 60, 90], gamma=0.1)
+else:
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[150, 180, 210], gamma=0.1)
         
 ################################
 #### 3.b Training 
